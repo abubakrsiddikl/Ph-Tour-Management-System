68a213420b97e210519a11cf863140304238146d
Abu Bakr Siddik <limonsk026@gmail.com>
2025-07-31 09:13:32 +0600

    part 6 complete

--- src/app/modules/tour/tour.service.ts
+++ src/app/modules/tour/tour.service.ts
@@ -1,28 +1,29 @@
-
+import { Query } from "mongoose";
 import { QueryBuilder } from "../../utils/QueryBuilder";
 import { tourSearchableFields } from "./tour.constant";
 import { ITour, ITourType } from "./tour.interface";
 import { Tour, TourType } from "./tour.model";
+import { excludeField } from "../../constants";
 
 const createTour = async (payload: ITour) => {
-    const existingTour = await Tour.findOne({ title: payload.title });
-    if (existingTour) {
-        throw new Error("A tour with this title already exists.");
-    }
+  const existingTour = await Tour.findOne({ title: payload.title });
+  if (existingTour) {
+    throw new Error("A tour with this title already exists.");
+  }
 
-    // const baseSlug = payload.title.toLowerCase().split(" ").join("-")
-    // let slug = `${baseSlug}`
+  // const baseSlug = payload.title.toLowerCase().split(" ").join("-")
+  // let slug = `${baseSlug}`
 
-    // let counter = 0;
-    // while (await Tour.exists({ slug })) {
-    //     slug = `${slug}-${counter++}` // dhaka-division-2
-    // }
+  // let counter = 0;
+  // while (await Tour.exists({ slug })) {
+  //     slug = `${slug}-${counter++}` // dhaka-division-2
+  // }
 
-    // payload.slug = slug;
+  // payload.slug = slug;
 
-    const tour = await Tour.create(payload)
+  const tour = await Tour.create(payload);
 
-    return tour;
+  return tour;
 };
 
 // const getAllToursOld = async (query: Record<string, string>) => {
@@ -43,16 +44,12 @@ const createTour = async (payload: ITour) => {
 //     // delete filter["searchTerm"]
 //     // delete filter["sort"]
 
-
-
 //     for (const field of excludeField) {
 //         // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
 //         delete filter[field]
 //     }
 
 //     console.log(filter);
-
-
 
 //     const searchQuery = {
 //         $or: tourSearchableFields.map(field => ({ [field]: { $regex: searchTerm, $options: "i" } }))
@@ -97,98 +94,93 @@ const createTour = async (payload: ITour) => {
 // };
 
 const getAllTours = async (query: Record<string, string>) => {
+  const queryBuilder = new QueryBuilder(Tour.find(), query);
 
+  const tours = await queryBuilder
+    .search(tourSearchableFields)
+    .filter()
+    .sort()
+    .paginate();
 
-    const queryBuilder = new QueryBuilder(Tour.find(), query)
+  // const meta = await queryBuilder.getMeta();
+  const [data, meta] = await Promise.all([
+    tours.build(),
+    queryBuilder.getMeta(),
+  ]);
 
-    const tours = await queryBuilder
-        .search(tourSearchableFields)
-        .filter()
-        .sort()
-        .fields()
-        .paginate()
-
-    // const meta = await queryBuilder.getMeta()
-
-    const [data, meta] = await Promise.all([
-        tours.build(),
-        queryBuilder.getMeta()
-    ])
-
-
-    return {
-        data,
-        meta
-    }
+  return {
+    data,
+    meta,
+  };
 };
 
-
 const updateTour = async (id: string, payload: Partial<ITour>) => {
+  const existingTour = await Tour.findById(id);
 
-    const existingTour = await Tour.findById(id);
+  if (!existingTour) {
+    throw new Error("Tour not found.");
+  }
 
-    if (!existingTour) {
-        throw new Error("Tour not found.");
-    }
+  // if (payload.title) {
+  //     const baseSlug = payload.title.toLowerCase().split(" ").join("-")
+  //     let slug = `${baseSlug}`
 
-    // if (payload.title) {
-    //     const baseSlug = payload.title.toLowerCase().split(" ").join("-")
-    //     let slug = `${baseSlug}`
+  //     let counter = 0;
+  //     while (await Tour.exists({ slug })) {
+  //         slug = `${slug}-${counter++}` // dhaka-division-2
+  //     }
 
-    //     let counter = 0;
-    //     while (await Tour.exists({ slug })) {
-    //         slug = `${slug}-${counter++}` // dhaka-division-2
-    //     }
+  //     payload.slug = slug
+  // }
 
-    //     payload.slug = slug
-    // }
+  const updatedTour = await Tour.findByIdAndUpdate(id, payload, { new: true });
 
-    const updatedTour = await Tour.findByIdAndUpdate(id, payload, { new: true });
-
-    return updatedTour;
+  return updatedTour;
 };
 
 const deleteTour = async (id: string) => {
-    return await Tour.findByIdAndDelete(id);
+  return await Tour.findByIdAndDelete(id);
 };
 
 const createTourType = async (payload: ITourType) => {
-    const existingTourType = await TourType.findOne({ name: payload.name });
+  const existingTourType = await TourType.findOne({ name: payload.name });
 
-    if (existingTourType) {
-        throw new Error("Tour type already exists.");
-    }
+  if (existingTourType) {
+    throw new Error("Tour type already exists.");
+  }
 
-    return await TourType.create({ name });
+  return await TourType.create({ name });
 };
 const getAllTourTypes = async () => {
-    return await TourType.find();
+  return await TourType.find();
 };
 const updateTourType = async (id: string, payload: ITourType) => {
-    const existingTourType = await TourType.findById(id);
-    if (!existingTourType) {
-        throw new Error("Tour type not found.");
-    }
+  const existingTourType = await TourType.findById(id);
+  if (!existingTourType) {
+    throw new Error("Tour type not found.");
+  }
 
-    const updatedTourType = await TourType.findByIdAndUpdate(id, payload, { new: true });
-    return updatedTourType;
+  const updatedTourType = await TourType.findByIdAndUpdate(id, payload, {
+    new: true,
+  });
+  return updatedTourType;
 };
 const deleteTourType = async (id: string) => {
-    const existingTourType = await TourType.findById(id);
-    if (!existingTourType) {
-        throw new Error("Tour type not found.");
-    }
+  const existingTourType = await TourType.findById(id);
+  if (!existingTourType) {
+    throw new Error("Tour type not found.");
+  }
 
-    return await TourType.findByIdAndDelete(id);
+  return await TourType.findByIdAndDelete(id);
 };
 
 export const TourService = {
-    createTour,
-    createTourType,
-    deleteTourType,
-    updateTourType,
-    getAllTourTypes,
-    getAllTours,
-    updateTour,
-    deleteTour,
+  createTour,
+  createTourType,
+  deleteTourType,
+  updateTourType,
+  getAllTourTypes,
+  getAllTours,
+  updateTour,
+  deleteTour,
 };
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,37 +1,43 @@
-import { Router } from "express"
-import { AuthRoutes } from "../modules/auth/auth.route"
-import { UserRoutes } from "../modules/user/user.route"
-import { DivisionRoutes } from "../modules/division/division.route"
-import { TourRoutes } from "../modules/tour/tour.route"
+import { Router } from "express";
+import { AuthRoutes } from "../modules/auth/auth.route";
+import { UserRoutes } from "../modules/user/user.route";
+import { DivisionRoutes } from "../modules/division/division.route";
+import { TourRoutes } from "../modules/tour/tour.route";
+import { BookingRoutes } from "../modules/booking/booking.route";
+import { PaymentRoutes } from "../modules/payment/payment.route";
 
-export const router = Router()
+export const router = Router();
 
 const moduleRoutes = [
-    {
-        path: "/user",
-        route: UserRoutes
-    },
-    {
-        path: "/auth",
-        route: AuthRoutes
-    },
-    {
-        path: "/division",
-        route: DivisionRoutes
-    },
-    {
-        path: "/tour",
-        route: TourRoutes
-    }
-    // {
-    //     path: "/tour",
-    //     route: TourRoutes
-    // },
-]
+  {
+    path: "/user",
+    route: UserRoutes,
+  },
+  {
+    path: "/auth",
+    route: AuthRoutes,
+  },
+  {
+    path: "/division",
+    route: DivisionRoutes,
+  },
+  {
+    path: "/tour",
+    route: TourRoutes,
+  },
+  {
+    path: "/booking",
+    route: BookingRoutes,
+  },
+  {
+    path: "/payment",
+    route: PaymentRoutes,
+  },
+];
 
 moduleRoutes.forEach((route) => {
-    router.use(route.path, route.route)
-})
+  router.use(route.path, route.route);
+});
 
 // router.use("/user", UserRoutes)
 // router.use("/tour", TourRoutes)
